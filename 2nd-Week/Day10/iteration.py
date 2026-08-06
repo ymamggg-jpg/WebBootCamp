12698534d57cec666da0bc7b4e35f28440ffa005
Ymam <ymamggg@gmail.com>
2026-08-06 21:13:27 +0300

    Add files via upload

---
+++ 2nd-Week/Day10/iteration.py
@@ -0,0 +1,88 @@
+
+#Lap 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+for attempts in range (5):
+    print(f"Attempts: {attempts+ 1 }") 
+
+#Lap 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+for num in range (2 , 11 , 2): #double steps
+    print(num)
+
+#Lap 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+for secondsToLaunch in range(10 , 0 , -1):
+    print(f"T-: {secondsToLaunch}")
+
+#Lap 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+course = "Python"
+
+for letter in course:
+    print(letter)
+
+#Lap 5 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+students = ["Ymam" , "Sara" , "Ali"]
+
+for student in students :
+    print (f"progressing student is : {student}")
+
+#Lap 6 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+for number in range(1 , 11):
+    if number % 2 == 0 :
+        print(f"{number}  is even")
+    else:
+        print(f"{number}  is odd")
+
+#Lap 7 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+numbers = [4 , 5 ,9 , 8 , 2]
+even_number = 0
+
+for num in numbers:
+    if num % 2 == 0:
+     even_number += 1
+
+print(f"Total even numbers is: {num}")
+
+
+#Lap 8 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+prices = [24 , 44 , 62 , 15]
+total = 0 
+
+for price in prices:
+    total += price
+
+print(f" Your total is: {total} VAT: {total * (15/100)}")
+                                  #       *1.15:.2f}") #two decimal points
+
+#Lap 9 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+count = 0
+
+while count <= 5:
+ count += 1
+ print(f"count... {count}")
+print("Loop completed")
+
+#Lap 10 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+message = print("please enter your age: ")
+age_text = input(message).strip()
+
+while not age_text.isdigit:
+   age_text = input(message).strip()
+
+age = int(age_text)
+print(f"you are : {age}")
+
+#Lap 11 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+password = "123"
+
+while password != "" :
+   print("please enter your password")
+   password = input("enter your password!")
+   
