fd99abe54bdb7df8711f028dd7bd2ae878cb389c
Ymam <ymamggg@gmail.com>
2026-09-06 18:10:36 +0300

    Delete 6th-Week directory

--- 6th-Week/Day30/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, ReportsView
-
-app_name = "dashboard"
-
-urlpatterns = [
-    path("", home, name="home"),
-    path("reports/", ReportsView.as_view(), name="reports"),
-]
